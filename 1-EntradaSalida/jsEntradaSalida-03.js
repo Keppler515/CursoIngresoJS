ee53c9ca66719e8ae0dbc102f6f17926c387b756
Keppler515 <68975256+Keppler515@users.noreply.github.com>
2021-07-06 18:12:58 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,8 +4,8 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	nombreIngresado=txtIdNombre.value; //En el archivo HTML el input que captura el nombre tiene el ID 'txtIdNombre'
+	alert(nombreIngresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,15 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
 function mostrar()
 {
-	alert("ok");
+
+	let datoIngresado;
+		
+	datoIngresado = prompt("Ingrese su nombre");
+
+	document.getElementById('txtIdNombre').value = datoIngresado;
+
 }
 
