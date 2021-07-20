6970b15fb34e94f159f83bd4ef202e251826271c
Keppler515 <68975256+Keppler515@users.noreply.github.com>
2021-07-19 22:47:42 -0300

    Add files via upload

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,83 @@
+/*
+AUTOR: Maximiliano Sosa
+
+
+Enunciado:
+Bienvenidos (SOLO SWITCH).
+una empresa de viajes le solicita ingresar que continente le gustaria visitar
+y la cantidad de días , la oferta dice que por día se cobra $100 , que se puede pagar de todas las maneras
+Si es América tiene un 50% de descuento y si ademas paga por débito se le agrega un 10% mas de descuento
+Si es África tiene un 60% de descuento y si además paga por mercadoPago o efectivo se le agrega un 15% mas de descuento
+Si es Europa tiene un 20% de descuento y si ademas paga por débito se le agrega un 15% , por mercadoPago un 10% y cualquier otro medio 5%
+cualquier otro continente tiene un recargo del 20%
+Curso de ingreso UTN FRA
+*/
+
+
 function mostrar()
 {
+
+
+var costoDia;
+var cantidadDias;
+var neto;
+var continente;
+var formaDePago;
+var precioFinal;
+
+
+costoDia = 100;
+cantidadDias = prompt('Ingrese la cantidad de días');
+continente = document.getElementById('Marca').value;
+neto = costoDia * cantidadDias;
+formaDePago = prompt('Ingrese forma de pago: "d": débito; "m": mercadoPago; "e": efectivo');
+
+
+switch (continente) {
+	case 'América':
+		switch (formaDePago) {
+			case 'd':
+				precioFinal = neto - neto * 0.60;
+				break;
+			default:
+				precioFinal = neto - neto * 0.50;
+				break;
+		}
+		break;
+
+	case 'África':
+		switch (formaDePago) {
+			case 'm':
+				precioFinal = neto - neto * 0.75;
+				break;
+			case 'e':
+				precioFinal = neto - neto * 0.75;
+				break;	
+			default:
+				precioFinal = neto - neto * 0.60;
+				break;
+		}
+		break;
+		
+	case 'Europa':
+		switch (formaDePago) {
+			case 'd':
+				precioFinal = neto - neto * 0.35;
+				break;
+			case 'm':
+				precioFinal = neto - neto * 0.30;	
+				break;
+			default:
+				precioFinal = neto - neto * 0.25;
+				break;
+		}
+		break;
+					
+	default:
+		precioFinal = neto + neto * 0.20;
+		break;
+}
+
+	alert(precioFinal);
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,49 @@
+/*
+AUTOR: Maximiliano Sosa
+
+
+Enunciado:
+Bienvenidos (IF).
+Pedir dos números y mostrar el resultado:
+Si son iguales los muestro concatenados.
+Si el primero es mayor, los resto,
+de lo contrario los sumo.
+Si la resta es mayor a 10 ,además de mostrar el resultado, muestro el mensaje
+"la resta es xxx y superó el 10".
+*/
+
+
+
 function mostrar()
 {
+	var numero1;
+	var numero2;
+	var resta;
+	var suma;
 
-}
+	numero1 = prompt('Ingrese el primer número');
+	numero2 = prompt('Ingrese el segundo número');
+	resta = numero1 - numero2;
+	suma = numero1 + numero2;
+
+	
+	if (numero1 == numero2) {
+		alert(numero1 + numero2);
+
+	} else if (numero1 > numero2) {
+		numero1 = parseInt(numero1);
+		numero2 = parseInt(numero2);
+
+		alert(resta); //ACA FUNCIONA SI LLAMO A LA VARIABLE 
+
+	} else {
+		numero1 = parseInt(numero1);
+		numero2 = parseInt(numero2);
+
+		alert(numero1 + numero2); //ACA SI LLAMO A LA VARIABLE suma EN VEZ DE SUMARSE SE CONCATENA
+	}
+	
+		if (resta > 10) {
+			alert('La resta es ' + resta + ' y superó el 10');
+		}
+}//FIN DE LA FUNCION
